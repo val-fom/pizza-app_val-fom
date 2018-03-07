32d04926892ead7ea16d7d215e6f8582d7570bec
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-03-07 10:15:24 +0200

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,82 +7,69 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var inProduction = (process.env.NODE_ENV === 'production');
 
 module.exports = {
-
 	entry: {
 		app: [
-		'./src/main.js',
-		'./src/main.scss'
+			'./src/main.js',
+			'./src/main.scss'
 		]
 	},
-
 	output: {
 		path: path.resolve(__dirname, './dist'),
 		filename: '[name].js'
 	},
-
-	devtool: "source-map", 
-
+	devtool: !inProduction && "source-map",
 	module: {
-		rules: [
-
-		{
-			test: /\.s[ac]ss$/,
-			use: ExtractTextPlugin.extract({
-				use: [
-				{
-					loader: 'css-loader', options: {
-						sourceMap: true
-					}
-				},
-				{
-					loader: 'sass-loader', options: {
-						sourceMap: true
-					}
-				}
-				],
-				fallback: 'style-loader'
-			})
-		},
-
-		{
-			test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-			loader: 'file-loader?name=fonts/[name].[ext]'
-		},
-
-		{ 
-			test: /\.js$/, 
-			exclude: /node_modules/, 
-			loader: "babel-loader"
-		}
-
+		rules: [{
+				test: /\.s[ac]ss$/,
+				use: ExtractTextPlugin.extract({
+					use: [{
+							loader: 'css-loader',
+							options: {
+								sourceMap: !inProduction
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: !inProduction
+							}
+						}
+					],
+					fallback: 'style-loader'
+				})
+			},
+			{
+				test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+				loader: 'file-loader?name=fonts/[name].[ext]'
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			}
 		]
 	},
-
 	plugins: [
-
-	new ExtractTextPlugin("[name].css"),
-
-	new webpack.LoaderOptionsPlugin({
-		minimize: inProduction
-	}),
-
-	new HtmlWebpackPlugin({
-		template: 'src/index.html'
-	}),
-
-	new CopyWebpackPlugin([
-	{
-		from: 'assets',
-		to: ''
-	}
-	])
-
-	]
+		new ExtractTextPlugin("[name].css"),
+		new webpack.LoaderOptionsPlugin({
+			minimize: inProduction
+		}),
+		new HtmlWebpackPlugin({
+			template: 'src/index.html'
+		}),
+		new CopyWebpackPlugin([{
+			from: 'assets',
+			to: ''
+		}]),
+	],
+	devServer: {
+		contentBase: path.join(__dirname, "dist"),
+		compress: true,
+		port: 9000
+	},
 };
-
 if (inProduction) {
 	module.exports.plugins.push(
 		new webpack.optimize.UglifyJsPlugin()
-		)
+	)
 }
-
