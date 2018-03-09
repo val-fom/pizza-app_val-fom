2a31acd031b378395b64214d59ec25b6b82ec683
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-03-09 11:49:45 +0200

    login-form styling

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -7,16 +7,25 @@ export default class LoginForm extends Component {
 		super();
 
 		this.host = document.createElement('div');
-		this.host.classList.add('login__container');
+		this.host.classList.add('login-form__container');
 	}
 
 	render() {
 		return `
-			<form class="login">
-				<input type="text" class="login__name" required>
-				<input type="password" class="login__password" required>
-				<input type="submit" value="Sign in">
-			</form>
+<form class="login-form" method="post">
+	<label for="name">Login:</label>
+	<input type="text" class="login-form__name" name="login" id="login" required>
+	<label for="password">Password:</label>
+	<input type="password" class="login-form__password" name="password"
+		id="password" required>
+	<div class="login-form__button-wrapper">
+		<input type="submit" class="button login-form__button button--sign-in"
+			value="Sign in">
+		<a href="#/register" class="button login-form__button button--sign-up">
+			Sign up
+		</a>
+	</div>
+</form>
 		`;
 	}
 }
