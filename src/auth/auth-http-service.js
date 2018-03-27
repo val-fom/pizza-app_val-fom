5aefae38543c1038a285333a8e442b90d5015a4c
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-03-27 13:31:42 +0300

    refactor headers

--- src/auth/auth-http-service.js
+++ src/auth/auth-http-service.js
@@ -3,22 +3,20 @@ import { AUTH_SERVICE } from './auth-login-service.js';
 
 
 class AuthHttpService {
-	get(url, /*headers*/) {
-		// if (!AUTH_SERVICE.isAuthorized) {
-		// 	throw new Error('Non-authorized request');
-		// }
-
-		const headers = new Headers();
-
-		headers.append('Authorization', `Bearer ${AUTH_SERVICE.token}`);
-		headers.append('content-type', 'application/json');
+	get(url/*, headers*/) {
+		if (!AUTH_SERVICE.isAuthorized) {
+			throw new Error('Non-authorized request');
+		}
 
 		return fetch(url, {
 			method: 'GET',
-			headers,
+			headers: {
+				'Authorization': `Bearer ${AUTH_SERVICE.token}`,
+				'content-type': 'application/json'
+			}
 		})
-		.then(status)
-		.then(json)
+			.then(status)
+			.then(json);
 	}
 
 	post() {
