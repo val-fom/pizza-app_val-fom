5cebd0649913fc46933b4caf98d77164e4dec421
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-04-17 17:00:22 +0300

    cleanup

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -12,23 +12,45 @@ export default class List extends Component {
 	}
 
 	render() {
-		if (!this.props) return '<p class="message--success">Loading...</p>';
+		if (!this.props) return `
+			<p class="message--success">
+				Loading...
+			</p>`;
 
 		const { pizzas } = this.props;
 		console.log('pizzas: ', pizzas);
+
+		if (!pizzas.length) return `
+			<p class="message--success">
+				Pizza queue is empty
+			</p>
+		`;
 
 		return pizzas.reduce((html, pizza, i) => {
 			const eta = (new Date(pizza.time_prepared) - Date.now()) / 60000;
 
 			return html += `
 				<article class="list__item">
-					<img src="${API_SERVICE.DOMAIN}/${pizza.img_url}" alt="" class="item__pic" crossOrigin>
-					<time class="item__order-time">${getTime(new Date(pizza.created_date))}</time >
-					<h3 class="item__number-in-queue">#${pizzas.length - i}</h3>
-					<time datetime="0h 1m 0s" class="item__eta">ETA:
-						${Math.floor(eta)} min
+					<img
+						src="${API_SERVICE.DOMAIN}/${pizza.img_url}" 
+						alt="pizza" 
+						class="item__pic" 
+						crossOrigin
+					>
+					<time class="item__order-time">
+						${getTime(new Date(pizza.created_date))}
+					</time >
+					<h3 class="item__number-in-queue">
+						#${i + 1}
+					</h3>
+					<time
+						datetime="PT${Math.floor(eta)}M"
+						class="item__eta">
+							ETA: ${Math.floor(eta)} min
 					</time>
-					<span class="item__price">$${pizza.price.toFixed(2)}</span>
+					<span class="item__price">
+						$${pizza.price.toFixed(2)}
+					</span>
 				</article >
 			`;
 		}, '');
